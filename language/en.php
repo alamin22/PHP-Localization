c9674b5c9768d86e2a31f574297492f3ecdb36fb
Md. Al-Amin <41773444+alamin22@users.noreply.github.com>
2019-08-08 23:33:14 -0700

    Languages files that localize

---
+++ language/en.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang=array(
+  
+  'title'=>'Localization',
+  'home'=>'HOME',
+  'travel'=>'MY TRAVEL',
+  'service'=>'SERVICE',
+  'about'=>'ABOUT US',
+  'help'=>'HELP',
+  'language'=>'Language',
+  'dear'=>'Dear Friend',
+  'describe'=>' Hi! Everybody How are you?This is the section we will test for php localization.Hope will you understand',
+
+
+);
+
+
+?>
